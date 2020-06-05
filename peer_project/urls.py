f5733912b95e1a347f08b991ec4f4ae0ff439595
ruth <ruthwanjiramugo@gmail.com>
2020-06-05 07:55:54 +0300

    add the register form without all validations yet

--- peer_project/urls.py
+++ peer_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls import url
+from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('register/', user_views.register,name='register'),
     url(r'',include('peer.urls')),
 ]
