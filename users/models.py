488257e625f635d74a3cbfbfb3f7433228054ba7
ruth <ruthwanjiramugo@gmail.com>
2020-06-05 15:57:29 +0300

    add the profile class and its first three attributes

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+# Create your models here.
+class Profile (models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_img= models.ImageField(default="default.jpg",upload_to='gala/')
 
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    
 # Create your models here.
