d4a8316c4be666f0b4e69bfecd11654f8df26986
ruth <ruthwanjiramugo@gmail.com>
2020-06-05 14:40:16 +0300

    add the login form with the styling of the crispy forms

--- peer_project/settings.py
+++ peer_project/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+CRISPY_TEMPLATE_PACK ='bootstrap4'
+
+
 
 STATIC_URL = '/static/'
 STATIC_URL = '/static/'
