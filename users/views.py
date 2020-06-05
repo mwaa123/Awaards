37f04f1bfbbba7913d7409c992c6a688e96d05dd
ruth <ruthwanjiramugo@gmail.com>
2020-06-05 09:26:37 +0300

    add the validation on registration and a pop up message

--- users/views.py
+++ users/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+# from django.contrib.auth.decorators import login_required
+# from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+# from django.core.mail import send_mail
+# from django.conf import settings
+# from django.contrib.auth.models import User
+# from django.db.models.signals import UserProfile
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            
+        return redirect('')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'find/register.html', {'form': form})
 
 def register(request):
     form =UserCreationForm()
