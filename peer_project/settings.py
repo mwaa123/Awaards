f335933ccf3a10a37419bdd272add5ab8814672b
ruth <ruthwanjiramugo@gmail.com>
2020-06-06 20:50:56 +0300

    add the ratingsinstallations

--- peer_project/settings.py
+++ peer_project/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.request',
             ],
         },
     },
@@ -135,3 +136,4 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
