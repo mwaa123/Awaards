174c6f70771ca789afb84c0b744dc62b86a25aaa
ruth <ruthwanjiramugo@gmail.com>
2020-06-06 07:53:04 +0300

    add the form in the profile page for its update

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-# from .models import Profile
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -11,7 +11,7 @@ class UserRegisterForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
 
-  class UserUpdateForm(forms.ModelForm):
+class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
     class Meta:
         model = User
